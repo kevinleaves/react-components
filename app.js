c336476fa15e96b08e8e1c6f6b92289bfb5dc7f2
Kevin Le <iamkevinvle@gmail.com>
2023-01-03 11:51:18 -0800

    complete react-components

--- app.js
+++ app.js
@@ -1,1 +1,37 @@
 // TODO
+const { useState } = React;
+
+const App = () => {
+
+  return (
+    <>
+      <h1>MY GROCERY LIST</h1>
+      <GroceryList items = {['Apples', 'Pineapples']}/>
+    </>
+  )
+}
+
+function GroceryList ({items, handleClick}) {
+  return (
+    <ul>
+      {items.map((item) => (
+        <GroceryListItem listItem={item}/>
+      ))}
+    </ul>
+  )
+}
+
+const GroceryListItem = ({listItem}) => {
+
+  const [isPurchased, setPurchased] = useState(false);
+
+  const style = {
+    textDecoration: isPurchased ? 'line-through' : 'none'
+  }
+
+  return (
+    <li style={style} onClick={() => {setPurchased(!isPurchased)}}>{listItem}</li>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('app'))
